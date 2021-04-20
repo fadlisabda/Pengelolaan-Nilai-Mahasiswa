fb7a6526bdb409963694139bddda0b4acb3d7c98
fadlisabda1 <fadlisabda1@yahoo.com>
2021-04-20 12:03:45 +0700

    menambahkan fitur database dan mt-3 pada jumbotron

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,4 +1,4 @@
-	<div class="jumbotron jumbotron-fluid">
+	<div class="jumbotron jumbotron-fluid mt-3">
 	  <div class="container">
 	    <h1 class="display-4">Hello <?= $nama; ?></h1>
 	    <p class="lead">Iam Learning Codeigniter 3</p>
