f942e639d8b3d3b674a19476689ad67cfc510038
fadlisabda1 <fadlisabda1@yahoo.com>
2021-04-20 10:08:31 +0700

    menampilkan nama di jumbotron

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,6 +1,6 @@
 	<div class="jumbotron jumbotron-fluid">
 	  <div class="container">
-	    <h1 class="display-4">Hello World</h1>
+	    <h1 class="display-4">Hello <?= $nama; ?></h1>
 	    <p class="lead">Iam Learning Codeigniter 3</p>
 	  </div>
 	</div>
