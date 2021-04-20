2c9b590351f6c7c4825f2585d6c1e39ac2c034a5
fadlisabda1 <fadlisabda1@yahoo.com>
2021-04-20 09:54:52 +0700

    menambahkan jumbotron

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,1 +1,6 @@
-	<h1>Hello, world!</h1>
+	<div class="jumbotron jumbotron-fluid">
+	  <div class="container">
+	    <h1 class="display-4">Hello World</h1>
+	    <p class="lead">Iam Learning Codeigniter 3</p>
+	  </div>
+	</div>
