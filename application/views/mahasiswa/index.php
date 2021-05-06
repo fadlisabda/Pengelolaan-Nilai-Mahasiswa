d9fa84909cd532d6ac5f679f9f3e03c8a477c7f0
fadlisabda1 <fadlisabda1@yahoo.com>
2021-05-07 05:43:27 +0700

    merubah unset data pada session menjadi unset data menggunakan script CI

--- application/views/mahasiswa/index.php
+++ application/views/mahasiswa/index.php
@@ -1,6 +1,6 @@
-<div class="container">	
+<div class="container">
 	<div class="flash-data" data-flashdata="<?= $this->session->flashdata('flash'); ?>"></div>
-	<?php unset($_SESSION['flash']); ?>
+	<?php $this->session->unset_userdata('flash'); ?>
 	<div class="row mt-3">
 		<div class="col-md-6">
 			<a href="<?= base_url(); ?>mahasiswa/tambah" class="btn btn-primary">Tambah Data Mahasiswa</a>
@@ -11,38 +11,37 @@
 		<div class="col-md-6">
 			<form action="<?= base_url('mahasiswa'); ?>" method="post">
 				<div class="input-group">
-				  <input type="text" class="form-control" placeholder="Cari Nama/Npm Mahasiswa.."
-				  name="cari" autocomplete="off" autofocus>
-				  <div class="input-group-append">
-				    <input class="btn btn-primary" type="submit" name="kirim">
-				  </div>
+					<input type="text" class="form-control" placeholder="Cari Nama/Npm Mahasiswa.." name="cari" autocomplete="off" autofocus>
+					<div class="input-group-append">
+						<input class="btn btn-primary" type="submit" name="kirim">
+					</div>
 				</div>
 			</form>
 		</div>
-	</div>	
+	</div>
 
 	<div class="row mt-3">
 		<div class="col-md-6">
 			<h3>Daftar Nilai Mahasiswa</h3>
 			<h5>Result : <?= $total_rows; ?></h5>
-			<?php if (empty($mahasiswa)): ?>
+			<?php if (empty($mahasiswa)) : ?>
 				<div class="alert alert-danger" role="alert">
-				  Data Mahasiswa Tidak Ditemukan
+					Data Mahasiswa Tidak Ditemukan
 				</div>
 			<?php endif; ?>
 			<ul class="list-group">
-			<?php foreach ($mahasiswa as $mhs): ?>
-				<li class="list-group-item">
-				    <?= ++$start; ?>
-				    <?= $mhs['nama']; ?>
-				    <div class="ms-2 me-auto">
-				  		<a href="<?= base_url(); ?>mahasiswa/detail/<?= $mhs['id']; ?>" class="badge badge-primary float-right" style="margin-left: 2px; height:20px; width: 30px;"><i class="fas fa-info"></i></a>
-				  		<a href="<?= base_url(); ?>mahasiswa/ubah/<?= $mhs['id']; ?>" class="badge badge-success float-right" style="margin-left: 2px; height:20px; width: 30px;"><i class="fas fa-user-edit"></i></a>
-				  		<a href="<?= base_url(); ?>mahasiswa/hapus/<?= $mhs['id']; ?>" class="badge badge-danger float-right tombol-hapus" style="height:20px; width: 30px;"><i class="far fa-trash-alt"></i></a>
-				  	</div>
-			  	</li>	  
-			<?php endforeach; ?> 
-			<?= $this->pagination->create_links(); ?>
+				<?php foreach ($mahasiswa as $mhs) : ?>
+					<li class="list-group-item">
+						<?= ++$start; ?>
+						<?= $mhs['nama']; ?>
+						<div class="ms-2 me-auto">
+							<a href="<?= base_url(); ?>mahasiswa/detail/<?= $mhs['id']; ?>" class="badge badge-primary float-right" style="margin-left: 2px; height:20px; width: 30px;"><i class="fas fa-info"></i></a>
+							<a href="<?= base_url(); ?>mahasiswa/ubah/<?= $mhs['id']; ?>" class="badge badge-success float-right" style="margin-left: 2px; height:20px; width: 30px;"><i class="fas fa-user-edit"></i></a>
+							<a href="<?= base_url(); ?>mahasiswa/hapus/<?= $mhs['id']; ?>" class="badge badge-danger float-right tombol-hapus" style="height:20px; width: 30px;"><i class="far fa-trash-alt"></i></a>
+						</div>
+					</li>
+				<?php endforeach; ?>
+				<?= $this->pagination->create_links(); ?>
 		</div>
 	</div>
 </div>
